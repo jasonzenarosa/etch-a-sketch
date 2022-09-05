6d081f33c36b282a769df084c5c2d18bd2197399
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-09-04 17:40:06 -0700

    16x16 canvas

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < 16; j++) {
+        let pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        row.appendChild(pixel);
+    }
+    container.appendChild(row);
+}
+
+const pixels = document.querySelectorAll('.pixel');
+pixels.forEach((pixel) => {
+    pixel.addEventListener('click', () => {
+        pixel.classList.add('activated');
+    });
+});
